cb297a0f38a01b9c2401a6de041765a53060055d
joris <joris.lambrechts@bib.kuleuven.be>
2015-07-06 10:49:01 +0200

    about text from simple page

--- themes/alamire/index.php
+++ themes/alamire/index.php
@@ -2,16 +2,15 @@
 <div id="primary">
     <p><a id="continue" href="<?php echo url('solr-search');?>">Continue to IDEM</a><br>
        <a href="<?php echo url('terms');?>">I agree to the terms
-           and conditions of use</a></p>
-    
+           and conditions of use</a></p>    
 </div><!-- end primary -->
 
 <div id="secondary">
     
     <h5 class="about-header">About</h5>
-    <p>Proin at justo eget velit dapibus tincidunt vel id nisl. Donec in dapibus dui, nec tempus tortor. Cras malesuada semper porta. Morbi pretium tellus vitae lectus ultricies ornare. Curabitur sodales ipsum erat, eget cursus diam ultrices vitae. Quisque ante nisi, maximus vel ultrices ut, luctus vel felis. Nulla non aliquet turpis. Sed et felis nisi. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
-    <p>Donec venenatis arcu at consectetur ornare. Nulla finibus massa at varius vulputate. Nam eget felis sit amet neque consectetur sodales. Aenean rutrum lacinia ornare. Vivamus euismod augue vel felis commodo, sed pulvinar velit aliquet. Integer augue nisl, feugiat in erat ac, tristique suscipit arcu. Aenean pulvinar enim vel euismod ultricies.</p>
-    <p>Donec auctor suscipit erat sit amet placerat. Suspendisse et tristique velit. Duis dignissim commodo sem, in ultricies ex congue nec. Maecenas nibh arcu, auctor sed pulvinar dapibus, suscipit eu nibh. Nulla eget neque nec nunc laoreet ornare. Donec sed fringilla ex.</p>
+    
+    <?php echo libis_get_simple_page_content('About');?>
+        
     <?php fire_plugin_hook('public_home', array('view' => $this)); ?>
 
 </div><!-- end secondary -->
