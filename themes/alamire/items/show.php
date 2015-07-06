504fdd15ba246e000f2c20193224a2d3dbd79c53
joris <joris.lambrechts@bib.kuleuven.be>
2015-07-06 14:40:06 +0200

    fix #13 (link komt van metadataveld 'Viewer link')

--- themes/alamire/items/show.php
+++ themes/alamire/items/show.php
@@ -180,7 +180,7 @@
          <ul class="item-pagination navigation">
          <li id="previous-item" class="previous"><?php echo link_to_previous_item_show('previous result'); ?></li>
          <li id="next-item" class="next"><?php echo link_to_next_item_show('next result'); ?></li>
-         <li><?php echo libis_get_image_link($item);?></li>
+         <li<a href="<?php echo metadata('item', array('Item Type Metadata','Viewer link')); ?>">images</a></li>
          <li><a href="<?php echo url('collectiveaccess') ?>">collective access</a></li>
          </ul>
     </div>
