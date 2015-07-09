987dc851f03cbf656ee84a2c0df3601f575b3674
joris <joris.lambrechts@bib.kuleuven.be>
2015-07-09 10:30:48 +0200

    fix #23 & fix #24

--- themes/alamire/index.php
+++ themes/alamire/index.php
@@ -31,4 +31,39 @@
     </div><!--end recent-items -->
     <?php endif; ?>
 </div>
+
+</div><!-- end content -->
+
+<footer>
+
+        <div id="contact-info">
+            <p>Alamire Foundation<br>
+            Research Group Musicology KU Leuven
+            </p>
+            CENTRALE BIBLIOTHEEK<br>
+            MGR. LADEUZEPLEIN 21<br>
+            PB 5591 - BE-3000 LEUVEN<br>
+            TEL +32 16 32 87 50<br>
+            info@alamirefoundation.be<br>
+            <a href="http://www.alamirefoundation.org">www.alamirefoundation.org</a>
+            <p>
+            HOUSE OF POLYPHONY<br>
+            ABDIJ VAN PARK 1<br>
+            BE-3001 LEUVEN<br>
+            TEL +32 16 38 92 85
+            </p>
+        </div>   
+               
+        
+        <?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = option('copyright')): ?>
+        <p><?php echo $copyright; ?></p>
+        <?php endif; ?>
+        <!--<nav><?php echo public_nav_main()->setMaxDepth(0); ?></nav>-->
+        
+
+
+     <?php fire_plugin_hook('public_footer', array('view'=>$this)); ?>
+
+</footer>
+</div>
 <?php echo foot(); ?>
