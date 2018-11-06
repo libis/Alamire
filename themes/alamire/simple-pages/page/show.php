ad482c45fa0fbb2565bc6ab1ab375874fe83bb88
Joris <joris.lambrechts@kuleuven.be>
2018-11-06 14:46:18 +0100

    layout + links carillon

--- themes/alamire/simple-pages/page/show.php
+++ themes/alamire/simple-pages/page/show.php
@@ -31,9 +31,8 @@
     <?php endif;?>
     <?php if($slug == "carillon"):?>
       <div id="side-gallery">
-        <img src="https://idemdatabase.org/files/thumbnails/bbdc7a0f55eb835e45cf13672fd2d47e.jpg">
-          <img src="https://idemdatabase.org/files/thumbnails/d98cc8558fca6bafc7cbde65b089854c.jpg">
-          <img src="https://idemdatabase.org/files/thumbnails/546dbbdf8821aac72b9054bbbfebed84.jpg">
+        <img src="https://idemdatabase.org/files/thumbnails/63c24736de39e75888937647452a98f4.jpg">
+        <img src="https://idemdatabase.org/files/thumbnails/63116d2d591a525efd4d438ddf767db4.jpg">
       </div>
     <?php endif;?>
 
